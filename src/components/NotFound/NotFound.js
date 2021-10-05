d3978f70e29b0e8a4234836ef403744385878f94
Ibrahim H <mohammad.ihakash@gmail.com>
2021-10-05 21:50:03 +0600

    Not found page design complete

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import './NotFound.css';
+import img404 from '../../images/404.jpg';
+import { NavLink } from 'react-router-dom';
 
 const NotFound = () => {
     return (
-        <div>
-            <h1>404 not found</h1>
+        <div className="notFound-container">
+            <div className="row">
+                <div className="col-md-10 col-12 mx-auto text-center">
+                    <img className="w-100" src={img404} alt="404 not found" />
+                </div>
+            </div>
+            <div className="text-center">
+                <NavLink to="/"><button className="btn special-bg fs-4 text-light fw-bold">Return</button></NavLink>
+            </div>
         </div>
     );
 };
