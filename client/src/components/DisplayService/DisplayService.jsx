070e8478f4a2ef31c9fb207eb838f484cac0319e
Md. Ibrahim Hossain <ibuhossain3@gmail.com>
2024-04-22 04:32:49 +0600

    updated

--- client/src/components/DisplayService/DisplayService.jsx
+++ client/src/components/DisplayService/DisplayService.jsx
@@ -1,32 +1,34 @@
-import React, { useEffect, useState } from 'react';
-import { Container } from 'react-bootstrap';
-import Course from '../Course/Course';
+import React, { useEffect, useState } from "react";
+import { Container } from "react-bootstrap";
+import Course from "../Course/Course";
 
 const DisplayService = () => {
-    const [services, setServices] = useState([]);
-    const tmpCourse = [];
-    useEffect(() => {
-        fetch('./courses.JSON')
-            .then(res => res.json())
-            .then(data => {
-                for (let i = 0; i < 4; i++)
-                    tmpCourse.push(data[i]);
-                setServices(tmpCourse);
-            });
-    }, []);
-    return (
-        <Container className="my-5">
-            <div className="text-center">
-                <h1>Popular courses this month</h1>
-                <p>Break into a new field like information technology or data science. No prior experience necessary to get started.</p>
-            </div>
-            <div className="d-flex flex-wrap justify-content-center">
-                {
-                    services.map(service => <Course key={service._id} service={service} />)
-                }
-            </div>
-        </Container>
-    );
+  const [services, setServices] = useState([]);
+  useEffect(() => {
+    fetch("./courses.JSON")
+      .then((res) => res.json())
+      .then((data) => {
+        const tmpCourse = [];
+        for (let i = 0; i < 4; i++) tmpCourse.push(data[i]);
+        setServices(tmpCourse);
+      });
+  }, []);
+  return (
+    <Container className="my-5">
+      <div className="text-center">
+        <h1>Popular courses this month</h1>
+        <p>
+          Break into a new field like information technology or data science. No
+          prior experience necessary to get started.
+        </p>
+      </div>
+      <div className="d-flex flex-wrap justify-content-center">
+        {services.map((service) => (
+          <Course key={service._id} service={service} />
+        ))}
+      </div>
+    </Container>
+  );
 };
 
 export default DisplayService;
